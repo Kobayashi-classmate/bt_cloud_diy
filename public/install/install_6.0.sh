17c307cd2daba70422d3f346d3daf5a705dccd1c
Random thoughts diary <107290433+Kobayashi-classmate@users.noreply.github.com>
2024-10-09 15:57:03 +0800

    modified:   public/install/install_6.0.sh

--- public/install/install_6.0.sh
+++ public/install/install_6.0.sh
@@ -987,7 +987,7 @@ Set_Bt_Panel(){
 		lsattr python3.7 python
 		Red_Error "ERROR: The BT-Panel service startup failed." "ERROR: 宝塔启动失败"
 	fi
-	sleep 5
+	sleep 15
 	if [ "$PANEL_USER" ];then
 		cd ${setup_path}/server/panel/
 		btpython -c 'import tools;tools.set_panel_username("'$PANEL_USER'")'
